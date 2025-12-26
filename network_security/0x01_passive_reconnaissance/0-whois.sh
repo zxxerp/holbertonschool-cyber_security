811b0603ee630156a921343fb3abed36d0cf790d
zxxerp <ayxanhuseynzade12@gmail.com>
2025-12-26 13:02:51 +0000

    a

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/{print $1 "," $2}' > "$1.csv"
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
